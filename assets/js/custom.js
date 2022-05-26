e6061077343302bbb6cb474b38e49a464e97c33b
Hazman <muhamad_hazman@oum.edu.my>
2022-05-26 21:24:57 +0800

    update code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,28 +13,6 @@ var $root = $('html, body');
 
 $(document).ready(function () {
 
-    //Cursor
-    var mouseCursor = $(".mouse-cursor");
-    if (mouseCursor.length) {
-        if ($("body")) {
-            const e = document.querySelector(".cursor"),
-                t = document.querySelector(".cursor-effect");
-            let n,
-                i = 0,
-                o = !1;
-            (window.onmousemove = function (s) {
-                o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), (e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), (n = s.clientY), (i = s.clientX);
-            }),
-                $("body").on("mouseenter", "a, .cursor-pointer", function () {
-                    e.classList.add("cursor-hover"), t.classList.add("cursor-hover");
-                }),
-                $("body").on("mouseleave", "a, .cursor-pointer", function () {
-                    ($(this).is("a") && $(this).closest(".cursor-pointer").length) || (e.classList.remove("cursor-hover"), t.classList.remove("cursor-hover"));
-                }),
-                (e.style.visibility = "visible"),
-                (t.style.visibility = "visible");
-        }
-    }
 
     //SmothScroll
     $('.header .navbar-nav a, .to-contact, .scroll-down a').on('click', function(event) {
@@ -82,88 +60,6 @@ $(document).ready(function () {
         });
     }
 
-
-
-
-    // Theme Option
-    
-    $('.color-scheme li .dark-scheme').on('click', function(){
-        $("body").addClass('westin-dark');
-        $('.color-scheme li a').removeClass('active');
-        $(this).addClass('active');
-        //Change the logo img property 
-        $("#logo").attr("src", 'assets/img/westin-logo-dark.png');
-    });
-
-    $('.color-scheme li .light-scheme').on('click', function(){
-        $("body").removeClass('westin-dark');
-        $('.color-scheme li a').removeClass('active');
-        $(this).addClass('active');
-        //Change the logo img property 
-        $("#logo").attr("src", 'assets/img/westin-logo.png');
-    });
-
-    $('.theme-skin li .border-skin').on('click', function(){
-        $("body").addClass('border-style-demo');
-        $('.theme-skin li a').removeClass('active');
-        $(this).addClass('active');
-    });
-
-    $('.theme-skin li .flat-skin').on('click', function(){
-        $("body").removeClass('border-style-demo');
-        $('.theme-skin li a').removeClass('active');
-        $(this).addClass('active');
-    });
-
-    $("ul.pattern .color1").on('click', function(){
-        return $("#option-color").attr("href", "assets/css/color/azure.css"), !1
-    });
-    $("ul.pattern .color2").on('click', function(){
-        return $("#option-color").attr("href", "assets/css/color/blue.css"), !1
-    });
-    $("ul.pattern .color3").on('click', function(){
-        return $("#option-color").attr("href", "assets/css/color/purple.css"), !1
-    });
-    $("ul.pattern .color4").on('click', function(){
-        return $("#option-color").attr("href", "assets/css/color/dark-purple.css"), !1
-    });
-    $("ul.pattern .color5").on('click', function(){
-        return $("#option-color").attr("href", "assets/css/color/golden.css"), !1
-    });
-    $("ul.pattern .color6").on('click', function(){
-        return $("#option-color").attr("href", "assets/css/color/green.css"), !1
-    });
-    $("ul.pattern .color7").on('click', function(){
-        return $("#option-color").attr("href", "assets/css/color/citrus.css"), !1
-    });
-    $("ul.pattern .color8").on('click', function(){
-        return $("#option-color").attr("href", "assets/css/color/light-orange.css"), !1
-    });
-    $("ul.pattern .color9").on('click', function(){
-        return $("#option-color").attr("href", "assets/css/color/orange.css"), !1
-    });
-    $("ul.pattern .color10").on('click', function(){
-        return $("#option-color").attr("href", "assets/css/color/red.css"), !1
-    });
-    $("ul.pattern .color11").on('click', function(){
-        return $("#option-color").attr("href", "assets/css/color/pink.css"), !1
-    });
-    $("ul.pattern .color12").on('click', function(){
-        return $("#option-color").attr("href", "assets/css/color/mono.css"), !1
-    });
-    $("#color-switcher .pallet-button").on('click', function(){
-        return $("#color-switcher .color-pallet").toggleClass('show'), !1
-    })
-    $("ul.pattern .color13").on('click', function(){
-        return $("#option-color-two").attr("href", "assets/css/color/pink.css"), !1
-    });
-    $("ul.pattern .color14").on('click', function(){
-        return $("#option-color-two").attr("href", "assets/css/color/sky-blue.css"), !1
-    });
-    $("ul.pattern .color15").on('click', function(){
-        return $("#option-color-two").attr("href", "assets/css/color/golden.css"), !1
-    });
-    
 
     //Typed Js
     var $element = $(".element");
@@ -227,7 +123,7 @@ $window.on("load", (function() {
          var $showMore = $('#showMore');
          $grid.isotope({
              itemSelector: '.portfolio-item',
-             layoutMode: 'masonry',
+             layoutMode: 'masonryHorizontal',
          });
          $pfilter.find('a').on("click",function() {
              var filterValue = $(this).attr('data-filter');
@@ -347,66 +243,3 @@ $window.on('scroll', function () {
     
 });
 
-
-function validateEmail(email) {
-
-
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
-function sendEmail() {
-
-    var name     = $('#name').val();
-    var family   = $('#family').val();
-    var email    = $('#email').val();
-    var phone    = $('#phone').val();
-    var comments = $('#comments').val();
-
-    if(!name){
-        $('#message').toast('show').addClass('bg-danger').removeClass('bg-success');
-        $('.toast-body').html('First Name is required');
-    } else if(!family){
-        $('#message').toast('show').addClass('bg-danger').removeClass('bg-success');
-        $('.toast-body').html('Last Name is required');
-    } else if(!email){
-        $('#message').toast('show').addClass('bg-danger').removeClass('bg-success');
-        $('.toast-body').html('Email is required');
-    } else if(!validateEmail(email)){
-        $('#message').toast('show').addClass('bg-danger').removeClass('bg-success');
-        $('.toast-body').html('Email is not valid');
-    } else if(!phone){
-        $('#message').toast('show').addClass('bg-danger').removeClass('bg-success');
-        $('.toast-body').html('phone is required');
-    }else if(!comments){
-        $('#message').toast('show').addClass('bg-danger').removeClass('bg-success');
-        $('.toast-body').html('Comments is required');
-    }else {
-        $.ajax({
-            type: 'POST',
-            data: $("#contactForm").serialize(),
-            url:  "sendEmail.php",
-            beforeSend: function() {
-                $('#submit-btn').html('<span class="spinner-border spinner-border-sm"></span> Loading..');
-            },
-            success: function(data) {
-                $('#submit-btn').html('Submit');
-                var myObj = JSON.parse(data);
-                if(myObj['status']=='Congratulation'){
-                    $('#message').toast('show').addClass('bg-success').removeClass('bg-danger bg-warning');
-                    $('.toast-body').html('<strong>'+ myObj['status'] +' : </strong> '+ myObj['message']);
-                }else if(myObj['status']=='Error'){
-                    $('#message').toast('show').addClass('bg-danger').removeClass('bg-success bg-warning');
-                    $('.toast-body').html('<strong>'+ myObj['status'] +' : </strong> '+ myObj['message']);
-                }else if(myObj['status']=='Warning'){
-                    $('#message').toast('show').addClass('bg-warning').removeClass('bg-success bg-danger');
-                    $('.toast-body').html('<strong>'+ myObj['status'] +' : </strong> '+ myObj['message']);
-                }
-            },
-            error: function(xhr) {
-                $('#submit-btn').html('Submit');
-                $('#message').toast('show').addClass('bg-danger').removeClass('bg-success bg-warning');
-                $('.toast-body').html('<strong> Error : </strong> Something went wrong, Please try again.');
-            },
-        });
-    }
-}
